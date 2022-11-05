ea833da1356edc6b49653beacb7cc46bcdb2f122
Giannis Tzonis <tzonis.john@hotmail.com>
2022-11-05 20:27:50 +0200

    added most recent notes on top of the note-list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Split from "react-split";
 import { nanoid } from "nanoid";
 
 export default function App() {
+  /**
+   * Challenge: When the user edits a note, reposition
+   * it in the list of notes to the top of the list
+   */
   const [notes, setNotes] = React.useState(
     () => JSON.parse(localStorage.getItem("notes")) || []
   );
@@ -27,13 +31,20 @@ export default function App() {
   }
 
   function updateNote(text) {
-    setNotes((oldNotes) =>
-      oldNotes.map((oldNote) => {
-        return oldNote.id === currentNoteId
-          ? { ...oldNote, body: text }
-          : oldNote;
-      })
-    );
+    // Try to rearrange the most recently-modified
+    // not to be at the top
+    setNotes((oldNotes) => {
+      const newArray = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text });
+        } else {
+          newArray.push(oldNote);
+        }
+      }
+      return newArray;
+    });
   }
 
   function findCurrentNote() {
